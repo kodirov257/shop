82f7cded1243a2c510cbe4c6af2a195085f2c417
a.kodirov <abdurahmonkodirov97@gmail.com>
2019-08-09 14:36:30 +0500

    added composite model

--- shop/forms/manage/Shop/BrandForm.php
+++ shop/forms/manage/Shop/BrandForm.php
@@ -5,19 +5,18 @@ namespace shop\forms\manage\Shop;
 
 
 use shop\entities\Shop\Brand;
+use shop\forms\CompositeForm;
 use shop\forms\manage\MetaForm;
-use yii\base\Model;
 
 /**
  *
  * @property MetaForm $meta
  */
-class BrandForm extends Model
+class BrandForm extends CompositeForm
 {
     public $name;
     public $slug;
 
-    private $_meta;
     private $_brand;
 
     public function __construct(Brand $brand = null, $config = [])
@@ -25,25 +24,13 @@ class BrandForm extends Model
         if ($brand) {
             $this->name = $brand->name;
             $this->slug = $brand->slug;
-            $this->_meta = new MetaForm($brand->meta);
+            $this->meta = new MetaForm($brand->meta);
             $this->_brand = $brand;
+        } else {
+            $this->meta = new MetaForm();
         }
 
         parent::__construct($config);
-    }
-
-    public function load($data, $formName = null): bool
-    {
-        $self = parent::load($data, $formName);
-        $meta = $this->_meta->load($data, $formName ? null : 'meta');
-        return $self && $meta;
-    }
-
-    public function validate($attributeNames = null, $clearErrors = true): bool
-    {
-        $self = parent::validate($attributeNames, $clearErrors);
-        $meta = $this->_meta->validate($attributeNames, $clearErrors);
-        return $self && $meta;
     }
 
     public function rules(): array
@@ -59,5 +46,10 @@ class BrandForm extends Model
     public function getMeta(): MetaForm
     {
         return $this->_meta;
+    }
+
+    protected function internalForms(): array
+    {
+        return ['meta'];
     }
 }
