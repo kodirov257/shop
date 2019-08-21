a98cf047d05f1f2080d5d17129ed0acb3fb442fe
a.kodirov <abdurahmonkodirov97@gmail.com>
2019-08-21 12:04:36 +0500

    added category removing restriction

--- shop/services/manage/Shop/CategoryManageService.php
+++ shop/services/manage/Shop/CategoryManageService.php
@@ -8,14 +8,17 @@ use shop\entities\Meta;
 use shop\entities\Shop\Category;
 use shop\forms\manage\Shop\CategoryForm;
 use shop\repositories\Shop\CategoryRepository;
+use shop\repositories\Shop\ProductRepository;
 
 class CategoryManageService
 {
     private $categories;
+    private $products;
 
-    public function __construct(CategoryRepository $categories)
+    public function __construct(CategoryRepository $categories, ProductRepository $products)
     {
         $this->categories = $categories;
+        $this->products = $products;
     }
 
     public function create(CategoryForm $form): Category
@@ -62,6 +65,10 @@ class CategoryManageService
     public function remove($id): void
     {
         $category = $this->categories->get($id);
+        $this->assertIsNotRoot($category);
+        if ($this->products->existsByMainCategory($category->id)) {
+            throw new \DomainException('Unable to remove category with products.');
+        }
         $this->categories->remove($category);
     }
 
