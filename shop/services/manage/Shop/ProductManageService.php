05ac50edfb28c2abfb5577955fd146aaa3660e10
a.kodirov <abdurahmonkodirov97@gmail.com>
2019-08-23 10:45:52 +0500

    added product description

--- shop/services/manage/Shop/ProductManageService.php
+++ shop/services/manage/Shop/ProductManageService.php
@@ -51,6 +51,7 @@ class ProductManageService
             $category->id,
             $form->code,
             $form->name,
+            $form->description,
             new Meta(
                 $form->meta->title,
                 $form->meta->description,
@@ -101,6 +102,7 @@ class ProductManageService
             $brand->id,
             $form->code,
             $form->name,
+            $form->description,
             new Meta(
                 $form->meta->title,
                 $form->meta->description,
